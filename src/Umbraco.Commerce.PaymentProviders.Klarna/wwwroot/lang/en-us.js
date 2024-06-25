96fd102a8d45e47c6ecc5411065fcfd4a7602d68
Dinh Tran <trd@umbraco.dk>
2024-06-25 11:17:18 +0700

    normalize translation keys

--- src/Umbraco.Commerce.PaymentProviders.Klarna/wwwroot/lang/en-us.js
+++ src/Umbraco.Commerce.PaymentProviders.Klarna/wwwroot/lang/en-us.js
@@ -1,70 +1,70 @@
 export default {
     paymentProviders: {
-        'klarna-hppLabel': 'Klarna (HPP)',
-        'klarna-hppDescription': 'Klarna payment provider using the Klarna Hosted Payment Page (HPP)',
-        'klarna-hppSettingsContinueUrlLabel': 'Continue URL',
-        'klarna-hppSettingsContinueUrlDescription': 'The URL to continue to after this provider has done processing. eg: /continue/',
-        'klarna-hppSettingsCancelUrlLabel': 'Cancel URL',
-        'klarna-hppSettingsCancelUrlDescription': 'The URL to return to if the payment attempt is canceled. eg: /cancel/',
-        'klarna-hppSettingsErrorUrlLabel': 'Error URL',
-        'klarna-hppSettingsErrorUrlDescription': 'The URL to return to if the payment attempt errors. eg: /error/',
+        'klarnaHppLabel': 'Klarna (HPP)',
+        'klarnaHppDescription': 'Klarna payment provider using the Klarna Hosted Payment Page (HPP)',
+        'klarnaHppSettingsContinueUrlLabel': 'Continue URL',
+        'klarnaHppSettingsContinueUrlDescription': 'The URL to continue to after this provider has done processing. eg: /continue/',
+        'klarnaHppSettingsCancelUrlLabel': 'Cancel URL',
+        'klarnaHppSettingsCancelUrlDescription': 'The URL to return to if the payment attempt is canceled. eg: /cancel/',
+        'klarnaHppSettingsErrorUrlLabel': 'Error URL',
+        'klarnaHppSettingsErrorUrlDescription': 'The URL to return to if the payment attempt errors. eg: /error/',
         
-        'klarna-hppSettingsBillingAddressLine1PropertyAliasLabel': 'Billing Address (Line 1) Property Alias',
-        'klarna-hppSettingsBillingAddressLine1PropertyAliasDescription': '[Required] The order property alias containing line 1 of the billing address',
+        'klarnaHppSettingsBillingAddressLine1PropertyAliasLabel': 'Billing Address (Line 1) Property Alias',
+        'klarnaHppSettingsBillingAddressLine1PropertyAliasDescription': '[Required] The order property alias containing line 1 of the billing address',
         
-        'klarna-hppSettingsBillingAddressLine2PropertyAliasLabel': 'Billing Address (Line 2) Property Alias',
-        'klarna-hppSettingsBillingAddressLine2PropertyAliasDescription': 'The order property alias containing line 2 of the billing address',
+        'klarnaHppSettingsBillingAddressLine2PropertyAliasLabel': 'Billing Address (Line 2) Property Alias',
+        'klarnaHppSettingsBillingAddressLine2PropertyAliasDescription': 'The order property alias containing line 2 of the billing address',
 
-        'klarna-hppSettingsBillingAddressCityPropertyAliasLabel': 'Billing Address City Property Alias',
-        'klarna-hppSettingsBillingAddressCityPropertyAliasDescription': '[Required] The order property alias containing the city of the billing address',
+        'klarnaHppSettingsBillingAddressCityPropertyAliasLabel': 'Billing Address City Property Alias',
+        'klarnaHppSettingsBillingAddressCityPropertyAliasDescription': '[Required] The order property alias containing the city of the billing address',
 
-        'klarna-hppSettingsBillingAddressStatePropertyAliasLabel': 'Billing Address State Property Alias',
-        'klarna-hppSettingsBillingAddressStatePropertyAliasDescription': 'The order property alias containing the state of the billing address',
+        'klarnaHppSettingsBillingAddressStatePropertyAliasLabel': 'Billing Address State Property Alias',
+        'klarnaHppSettingsBillingAddressStatePropertyAliasDescription': 'The order property alias containing the state of the billing address',
 
-        'klarna-hppSettingsBillingAddressZipCodePropertyAliasLabel': 'Billing Address ZipCode Property Alias',
-        'klarna-hppSettingsBillingAddressZipCodePropertyAliasDescription': '[Required] The order property alias containing the zip code of the billing address',
+        'klarnaHppSettingsBillingAddressZipCodePropertyAliasLabel': 'Billing Address ZipCode Property Alias',
+        'klarnaHppSettingsBillingAddressZipCodePropertyAliasDescription': '[Required] The order property alias containing the zip code of the billing address',
 
-        'klarna-hppSettingsApiRegionLabel': 'API Region',
-        'klarna-hppSettingsApiRegionDescription': 'The Klarna API Region to use',
+        'klarnaHppSettingsApiRegionLabel': 'API Region',
+        'klarnaHppSettingsApiRegionDescription': 'The Klarna API Region to use',
 
-        'klarna-hppSettingsTestApiUsernameLabel': 'Test API Username',
-        'klarna-hppSettingsTestApiUsernameDescription': 'The Username to use when connecting to the test Klarna API',
+        'klarnaHppSettingsTestApiUsernameLabel': 'Test API Username',
+        'klarnaHppSettingsTestApiUsernameDescription': 'The Username to use when connecting to the test Klarna API',
 
-        'klarna-hppSettingsTestApiPasswordLabel': 'Test API Password',
-        'klarna-hppSettingsTestApiPasswordDescription': 'The Password to use when connecting to the test Klarna API',
+        'klarnaHppSettingsTestApiPasswordLabel': 'Test API Password',
+        'klarnaHppSettingsTestApiPasswordDescription': 'The Password to use when connecting to the test Klarna API',
 
-        'klarna-hppSettingsLiveApiUsernameLabel': 'Live API Username',
-        'klarna-hppSettingsLiveApiUsernameDescription': 'The Username to use when connecting to the live Klarna API',
+        'klarnaHppSettingsLiveApiUsernameLabel': 'Live API Username',
+        'klarnaHppSettingsLiveApiUsernameDescription': 'The Username to use when connecting to the live Klarna API',
 
-        'klarna-hppSettingsLiveApiPasswordLabel': 'Live API Password',
-        'klarna-hppSettingsLiveApiPasswordDescription': 'The Password to use when connecting to the live Klarna API',
+        'klarnaHppSettingsLiveApiPasswordLabel': 'Live API Password',
+        'klarnaHppSettingsLiveApiPasswordDescription': 'The Password to use when connecting to the live Klarna API',
 
-        'klarna-hppSettingsCaptureLabel': 'Capture',
-        'klarna-hppSettingsCaptureDescription': 'Flag indicating whether to immediately capture the payment, or whether to just authorize the payment for later (manual) capture',
+        'klarnaHppSettingsCaptureLabel': 'Capture',
+        'klarnaHppSettingsCaptureDescription': 'Flag indicating whether to immediately capture the payment, or whether to just authorize the payment for later (manual) capture',
 
-        'klarna-hppSettingsTestModeLabel': 'Test Mode',
-        'klarna-hppSettingsTestModeDescription': 'Set whether to process payments in test mode',
+        'klarnaHppSettingsTestModeLabel': 'Test Mode',
+        'klarnaHppSettingsTestModeDescription': 'Set whether to process payments in test mode',
 
         // ===================
         // Advanced
         // ===================
 
-        'klarna-hppSettingsPaymentPageLogoUrlLabel': 'Payment Page Logo Url',
-        'klarna-hppSettingsPaymentPageLogoUrlDescription': 'Fully qualified URL of a logo image to display on the payment page',
+        'klarnaHppSettingsPaymentPageLogoUrlLabel': 'Payment Page Logo Url',
+        'klarnaHppSettingsPaymentPageLogoUrlDescription': 'Fully qualified URL of a logo image to display on the payment page',
 
-        'klarna-hppSettingsPaymentPagePageTitleLabel': 'Payment Page Page Title',
-        'klarna-hppSettingsPaymentPagePageTitleDescription': 'A custom title to display on the payment page',
+        'klarnaHppSettingsPaymentPagePageTitleLabel': 'Payment Page Page Title',
+        'klarnaHppSettingsPaymentPagePageTitleDescription': 'A custom title to display on the payment page',
 
-        'klarna-hppSettingsProductTypePropertyAliasLabel': 'Product Type Property Alias',
-        'klarna-hppSettingsProductTypePropertyAliasDescription': 'The order line property alias containing the type of the product. Property value can be one of either \'physical\' or \'digital\'',
+        'klarnaHppSettingsProductTypePropertyAliasLabel': 'Product Type Property Alias',
+        'klarnaHppSettingsProductTypePropertyAliasDescription': 'The order line property alias containing the type of the product. Property value can be one of either \'physical\' or \'digital\'',
 
-        'klarna-hppSettingsPaymentMethodCategoriesLabel': 'Payment Method Categories',
-        'klarna-hppSettingsPaymentMethodCategoriesDescription': 'Comma separated list of payment method categories to show on the payment page. If empty, all allowable options will be presented. Options are DIRECT_DEBIT, DIRECT_BANK_TRANSFER, PAY_NOW, PAY_LATER and PAY_OVER_TIME',
+        'klarnaHppSettingsPaymentMethodCategoriesLabel': 'Payment Method Categories',
+        'klarnaHppSettingsPaymentMethodCategoriesDescription': 'Comma separated list of payment method categories to show on the payment page. If empty, all allowable options will be presented. Options are DIRECT_DEBIT, DIRECT_BANK_TRANSFER, PAY_NOW, PAY_LATER and PAY_OVER_TIME',
 
-        'klarna-hppSettingsPaymentMethodCategoryLabel': 'Payment Method Category',
-        'klarna-hppSettingsPaymentMethodCategoryDescription': 'The payment method category to show on the payment page. Options are DIRECT_DEBIT, DIRECT_BANK_TRANSFER, PAY_NOW, PAY_LATER and PAY_OVER_TIME',
+        'klarnaHppSettingsPaymentMethodCategoryLabel': 'Payment Method Category',
+        'klarnaHppSettingsPaymentMethodCategoryDescription': 'The payment method category to show on the payment page. Options are DIRECT_DEBIT, DIRECT_BANK_TRANSFER, PAY_NOW, PAY_LATER and PAY_OVER_TIME',
 
-        'klarna-hppSettingsEnableFallbacksLabel': 'Enable Fallbacks',
-        'klarna-hppSettingsEnableFallbacksDescription': 'Set whether to fallback to other payment options if the initial payment attempt fails before redirecting back to the site',
+        'klarnaHppSettingsEnableFallbacksLabel': 'Enable Fallbacks',
+        'klarnaHppSettingsEnableFallbacksDescription': 'Set whether to fallback to other payment options if the initial payment attempt fails before redirecting back to the site',
     },
 };
